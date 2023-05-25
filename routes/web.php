430f07e89b01f0c808704eb5a1c56a9af9f89df8
syamsiyyatulftr <126143686+syamsiyyatulftr@users.noreply.github.com>
2023-05-26 06:15:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\LandingController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +19,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/landing', function () {
+//     return view('landing');
+// });
 
-Route::get('/user', [UserController::class, 'index']);
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
 // Route::get('/user', [UserController::class, 'create']);
+
+// ketika sudah mengelink jangan lupa untuk melakukan ->name('xxxxhalaman yang akan ditujuxxx')
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+
+Route::get('/role', [RoleController::class, 'index'])->name('role.index');
+
+// Route::get('/', [LandingController::class, 'index'])->name('landing');
+
+Route::get('/landing', [LandingController::class, 'index'])->name('landing');
