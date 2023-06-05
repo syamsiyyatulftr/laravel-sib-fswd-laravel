468f3c11e09dea121489bedb0b831b9112cd6896
syamsiyyatulftr <126143686+syamsiyyatulftr@users.noreply.github.com>
2023-06-06 01:14:22 +0700

    Update index.blade.php

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -5,6 +5,8 @@
                     <div class="container-fluid px-4">
                         <h1 class="mt-4">Product</h1>
                         
+                        <a class="btn btn-primary mb-2" href="{{ route('product.create') }}" role="button">Create New</a>
+
                         <div class="card mb-4">
                             <div class="card-body">
                                 <table id="datatablesSimple">
@@ -15,7 +17,7 @@
                                             <th>Nama</th>
                                             <th>Price</th>
                                             <th>Sale Price</th>
-                                            <th>Brand</th>
+                                            <th>Image</th>
                                             <th>Action</th>
                                            
                                         </tr>
@@ -25,17 +27,30 @@
                                    <tbody>
                                     {{-- @foreach ini adalah perulangan/pengulangan dari data yang diberikan disini data yang 
                                         dimaksud ['data' => $products] yang ada di ProductController--}}
-                                        @foreach ($data as $product) {{--yang dipanggil nama key nya dan harus di aliaskan key nya--}}
+                                        @foreach ($products as $product) {{--yang dipanggil nama key nya dan harus di aliaskan key nya--}}
                                             <tr>
                                                 <td>{{ $loop->iteration}}</td>
-                                                <td>{{ $product['category'] }}</td>
-                                                <td>{{ $product['name'] }}</td>
-                                                <td>{{ $product['price'] }}</td>
-                                                <td>{{ $product['sale_price'] }}</td>
-                                                <td>{{ $product['brands'] }}</td>
+                                                <td>{{ $product->category->name }}</td>
+                                                <td>{{ $product->name }}</td>
+                                                <td>Rp. {{ number_format ($product->price, 0, 2) }}</td>
+                                                <td>Rp. {{ number_format ($product->sale_price, 0, 2) }}</td>
+                                               
                                                 <td>
-                                                    <a href="#" class="btn btn-warning">Edit</a>
-                                                    <button class="btn btn-danger">Delete</button>
+                                                    @if ($product->image == null)
+                                                        <span class="badge bg-primary">No Image</span>
+                                                    @else
+                                                        <img src="{{ asset('storage/product/' . $product->image) }}" alt="{{ $product->name }}" style="max-width: 50px">
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    <form onsubmit="return confirm('Apakah Anda yakin untuk menghapus data ini?');" action="{{ route('product.destroy', $product->id) }}" method="POST">
+                                                        <a href="{{ route('product.edit', $product->id) }}" class="btn btn-sm btn-warning">Edit</a>
+                                                        @csrf
+                                                        @method('DELETE')
+
+                                                        <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+ 
+                                                    </form>
                                                 </td>
                                             </tr>    
                                         @endforeach
