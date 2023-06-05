0bf0ca0b9b8d45b021ba30842b9221d7650808bb
syamsiyyatulftr <126143686+syamsiyyatulftr@users.noreply.github.com>
2023-06-06 00:58:57 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,27 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-   public function index()
-   {
-        return view('user.index');
-   }
+    public function index()
+    {
+        // Ambil semua data user dari database
+        $users = User::with('role')->get();
+        // dd($users);
+        // Tampilkan halaman index
+        return view('user.index', compact('users'));
+    }
 
     public function create()
     {
-        return view('user.create');
+        // Ambil data roles dari database
+        $roles = Role::all();
+
+        // Tampilkan form create user dengan passing data roles
+        return view('user.create', compact('roles'));
     }
 
-    public function edit()
+    public function store(Request $request)
     {
-        return view('user.edit');
+        // Simpan data ke database
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'role_id' => $request->role,
+            'password' => bcrypt('password') // default password, sementara di hardcode
+        ]);
+
+        // Redirect ke halaman user.index
+        return redirect()->route('user.index');
     }
 
-    public function delete()
+    public function edit($id)
     {
-        return view('user.delete');
+        // Ambil data user berdasarkan id
+        $user = User::find($id);
+
+        // Ambil data roles dari database
+        $roles = Role::all();
+
+        // Tampilkan halaman edit dengan passing data user dan roles
+        return view('user.edit', compact('user', 'roles'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Ambil data user berdasarkan id
+        $user = User::find($id);
+        // dd($user);
+
+        // Update data user
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'role_id' => $request->role
+        ]);
+
+        // Redirect ke halaman user.index
+        return redirect()->route('user.index');
+    }
+
+    public function destroy($id)
+    {
+        // Ambil data user berdasarkan id
+        $user = User::find($id);
+
+        // Hapus data user
+        $user->delete();
+
+        // Redirect ke halaman user.index
+        return redirect()->route('user.index');
     }
 }
