982507de89ade11e5e8140eb95c83384f3ac3bac
syamsiyyatulftr <126143686+syamsiyyatulftr@users.noreply.github.com>
2023-06-06 00:56:37 +0700

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,104 +2,137 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
+
+
     public function index()
     {
-        $products = [
-            [
-                "id" => "29c3a6be-c042-4efd-a199-7d45b99fdcc2",
-                "category" => "cellphone",
-                "name" => "Apple iPhone 11 64GB Black",
-                "price" => 8900000,
-                "sale_price" => 7249000,
-                "brands" => "Apple",
-                "rating" => 4
-            ],
-            [
-                "id" => "105aea64-0b01-4883-b536-9889f06498d7",
-                "category" => "cellphone",
-                "name" => "Samsung Galaxy A14 5G 6/128GB",
-                "price" => 2900000,
-                "sale_price" => 0,
-                "brands" => "Samsung",
-                "rating" => 4
-            ],
-            [
-                "id" => "bb2d6c9e-4a86-485c-bde6-2a83c2b30dd5",
-                "category" => "tablet",
-                "name" => "Apple iPad (Gen 9) Wi-Fi 64GB",
-                "price" => 6299000,
-                "sale_price" => 5699000,
-                "brands" => "Apple",
-                "rating" => 5
-            ],
-            [
-                "id" => "c2209c46-99b1-4cf5-a1cb-243c4d6ff3bb",
-                "category" => "wearable",
-                "name" => "Apple Watch Series 7 45mm",
-                "price" => 8499000,
-                "sale_price" => 0,
-                "brands" => "Apple",
-                "rating" => 3
-            ],
-            [
-                "id" => "8fc7a400-d1e4-4fa1-926a-2ecfdc3266d4",
-                "category" => "laptop",
-                "name" => "ASUS VIVOBOOK M1403QA",
-                "price" => 8998000,
-                "sale_price" => 8398000,
-                "brands" => "Asus",
-                "rating" => 4
-            ],
-            [
-                "id" => "976448e1-8e60-48a3-b379-1d1e6973c268",
-                "category" => "accessories",
-                "name" => "Logitech K380 Keyboard Wireless Bluetooth",
-                "price" => 599000,
-                "sale_price" => 465000,
-                "brands" => "Logitech",
-                "rating" => 4
-            ],
-            [
-                "id" => "5f6f1894-f66b-438e-9e12-964273a76f3d",
-                "category" => "laptop",
-                "name" => "Lenovo V14 G2 AMD Ryzen 3",
-                "price" => 7999000,
-                "sale_price" => 6667000,
-                "brands" => "Lenovo",
-                "rating" => 5
-            ],
-            [
-                "id" => "f73bf72e-73c9-4f33-b5f3-9746a4cf6112",
-                "category" => "wearable",
-                "name" => "Samsung Galaxy Watch5 44mm",
-                "price" => 3999000,
-                "sale_price" => 3709000,
-                "brands" => "Samsung",
-                "rating" => 4
-            ],
-            [
-                "id" => "68f00d3c-cf44-46e6-816c-57fdc82f9eae",
-                "category" => "tablet",
-                "name" => "Samsung Galaxy Tab A7 Lite 3/32GB",
-                "price" => 2499000,
-                "sale_price" => 2229000,
-                "brands" => "Samsung",
-                "rating" => 5
-            ],
-            [
-                "id" => "bf572054-0d4b-4ada-abe8-01021daac303",
-                "category" => "accessories",
-                "name" => "Baseus Bowie WM05 True Wireless Bluetooth",
-                "price" => 1489000,
-                "sale_price" => 499000,
-                "brands" => "Baseus",
-                "rating" => 5
-            ]
-        ];
-        return view('product.index' , ['data' => $products]);
+        $products = Product::with('category')->get(); // pakai with karena kita sekalian mau mengambil kategori sesuai relasi dan ('category') harus sama dengan yang di model bagian public functionnya
+
+        if (Auth::user()->role->name == 'User') {
+            return view('product.card', ['products' => $products]);    
+        } else {
+            return view('product.index', ['products' => $products]);
+        }
+
+        // return view('product.index' , ['data' => $products]);
     }  
+
+     public function create()
+     {
+        $brands = Brand::all();
+        $categories = Category::all();
+
+        return view('product.create', compact('brands', 'categories') );
+     }
+    
+
+    public function store(Request $request)
+    {
+        //ini gunanya jika tabel tidak di isi maka tabel akan menggeluarkan notifikasi bahwa tabel tsb harus diisi
+        $validator = Validator::make($request->all(), [
+            'category' => 'required',
+            'name' => 'required|string|min:3',
+            'price' => 'required|integer',
+            'sale_price' => 'required|integer',
+            'brand' => 'required|string',
+            'image' => 'required|image|mimes:jpeg, png,jpg|max:2048'
+        ]);
+
+        if ($validator->fails()) {
+            // dd($validator->errors());
+            return redirect()->back()->withErrors($validator->errors())->withInput();  //withInput ini berguna ketika kita tidak sengaja mengosongkan tabel lalu kita tekan tombol submit maka data yg lain yg sudah terisi dia tidak akan ikut ter-reset jadi masih ada di dalam tabel dg menggunakan helper OLD yg ada di viewsnya
+        }
+
+        // ubah nama file
+        $imageName = time() . '.' . $request->image->extension();
+
+        // simpan file ke folder public/product
+        Storage::putFileAs('public/product', $request->image, $imageName);
+
+        $product = Product::create([
+            'category_id' => $request->category,
+            'name' => $request->name,
+            'price' => $request->price,
+            'sale_price' => $request->sale_price,
+            'brands' => $request->brand,
+            'image' => $imageName
+        ]);
+
+        return redirect()->route('product.index');
+    }
+
+    public function edit($id)
+    {
+        // ambil data product berdasarkan id
+        $product = Product::where('id', $id)->with('category')->first();
+
+        // ambil data brand dan category sebagai isian di pilihan (select)
+        $brands = Brand::all();
+        $categories = Category::all();
+
+        // tampilkan view edit dan passing data product
+        return view('product.edit', compact('product', 'brands', 'categories'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        // cek jika user mengupload gambar di form
+        if ($request->hasFile('image')) {
+            // ambil nama file gambar lama dari database
+            $old_image = Product::find($id)->image;
+
+            // hapus file gambar lama dari folder slider
+            Storage::delete('public/product/' . $old_image);
+
+            // ubah nama file
+            $imageName = time() . '.' . $request->image->extension();
+
+            // simpan file ke folder public/product
+            Storage::putFileAs('public/product', $request->image, $imageName);
+
+            // update data product
+            Product::where('id', $id)->update([
+                'category_id' => $request->category,
+                'name' => $request->name,
+                'price' => $request->price,
+                'sale_price' => $request->sale_price,
+                'brands' => $request->brand,
+                'image' => $imageName,
+            ]);
+        } else {
+            // update data product tanpa menyertakan file gambar
+            Product::where('id', $id)->update([
+                'category_id' => $request->category,
+                'name' => $request->name,
+                'price' => $request->price,
+                'sale_price' => $request->sale_price,
+                'brands' => $request->brand,
+            ]);
+        }
+
+        // redirect ke halaman product.index
+        return redirect()->route('product.index');
+    }
+
+    public function destroy($id)
+    {
+        // ambil data product berdasarkan id
+        $product = Product::find($id);
+
+        // hapus data product
+        $product->delete();
+
+        // redirect ke halaman product.index
+        return redirect()->route('product.index');
+    }
 }
